105d69f8ee8bb46780197b7c575fa1f211033a4a
oumnia sadaouni <sadaouni.oumnia@gmail.com>
2021-09-01 12:20:48 +0200

    created a front connectect to our api and displaying a prediction

--- app.py
+++ app.py
@@ -1,113 +1,66 @@
 import streamlit as st
 from numpy import load
 from numpy import expand_dims
-from matplotlib import pyplot
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os
+import requests
+import io
 
-st.header("Generate ASCII images using GAN")
-st.write("Choose any image and get corresponding ASCII art:")
+#st.header("Generate facades images using cGAN")
+st.markdown(
+    "<h1 style='text-align: center; color: blue;'>Generate facades images using cGAN</h1>",
+    unsafe_allow_html=True)
+#st.write("Choose any paint image and get the corresponding facade :")
+st.markdown(
+    "<h3 style='text-align: center; color: ivory;'>Choose any paint image and get the corresponding facade :</h3>",
+    unsafe_allow_html=True)
+st.balloons()
 
-uploaded_file = st.file_uploader("Choose an image...")
+st.set_option("deprecation.showfileUploaderEncoding", False)
+uploaded_file = st.file_uploader("Choose an image", type="png")
 
-# def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
+if uploaded_file is not None:
 
-#     # The array of ascii symbols from white to black
-#     chars = np.asarray(list(' .,:irs?@9B&#'))
+    # uploaded_file.__dict__
+    # uploaded_file
 
-#     # Load the fonts and then get the the height and width of a typical symbol
-#     # You can use different fonts here
-#     font = ImageFont.load_default()
-#     letter_width = font.getsize("x")[0]
-#     letter_height = font.getsize("x")[1]
-
-#     WCF = letter_height/letter_width
-
-#     #open the input file
-#     img = Image.open(in_f)
-
-
-#     #Based on the desired output image size, calculate how many ascii letters are needed on the width and height
-#     widthByLetter=round(img.size[0]*SC*WCF)
-#     heightByLetter = round(img.size[1]*SC)
-#     S = (widthByLetter, heightByLetter)
-
-#     #Resize the image based on the symbol width and height
-#     img = img.resize(S)
-
-#     #Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
-#     # Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
-#     img = np.sum(np.asarray(img), axis=2)
-
-#     # Normalize the results, enhance and reduce the brightness contrast.
-#     # Map grayscale values to bins of symbols
-#     img -= img.min()
-#     img = (1.0 - img/img.max())**GCF*(chars.size-1)
-
-#     # Generate the ascii art symbols
-#     lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
-
-#     # Create gradient color bins
-#     nbins = len(lines)
-#     #colorRange =list(Color(color1).range_to(Color(color2), nbins))
-
-#     #Create an image object, set its width and height
-#     newImg_width= letter_width *widthByLetter
-#     newImg_height = letter_height * heightByLetter
-#     newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
-#     draw = ImageDraw.Draw(newImg)
-
-#     # Print symbols to image
-#     leftpadding=0
-#     y = 0
-#     lineIdx=0
-#     for line in lines:
-#         color = 'blue'
-#         lineIdx +=1
-
-#         draw.text((leftpadding, y), line, '#0000FF', font=font)
-#         y += letter_height
-
-#     # Save the image file
-
-#     #out_f = out_f.resize((1280,720))
-#     newImg.save(out_f)
+    image = Image.open(uploaded_file)
+    #st.image(image, caption="Uploaded image", use_column_width=True)
+    col1, col2 = st.columns(2)
+    col1.header("Input Image")
+    col1.image(image, use_column_width=True)
+    col2.header("Output Image")
+    col2.image(image, use_column_width=True)
 
 
-# def load_image(filename, size=(512,512)):
-# 	# load image with the preferred size
-# 	pixels = load_img(filename, target_size=size)
-# 	# convert to numpy array
-# 	pixels = img_to_array(pixels)
-# 	# scale from [0,255] to [-1,1]
-# 	pixels = (pixels - 127.5) / 127.5
-# 	# reshape to 1 sample
-# 	pixels = expand_dims(pixels, 0)
-# 	return pixels
 
+    # convert image to bytes
+    img_byte_arr = io.BytesIO()
+    image.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
 
-# def imgGen2(img1):
-#   inputf = img1  # Input image file name
+    # with open("image.jpg", "wb") as f:
+    #     f.write(img_byte_arr)
 
-#   SC = 0.1    # pixel sampling rate in width
-#   GCF= 2      # contrast adjustment
+    # api call
+    url = "https://pix2pix-dttdfzxwga-ew.a.run.app/predict"
+    files = {"file": img_byte_arr}
 
-#   asciiart(inputf, SC, GCF, "results.png")   #default color, black to blue
-#   asciiart(inputf, SC, GCF, "results_pink.png","blue","pink")
-#   img = Image.open(img1)
-#   img2 = Image.open('results.png').resize(img.size)
-#   #img2.save('result.png')
-#   #img3 = Image.open('results_pink.png').resize(img.size)
-#   #img3.save('resultp.png')
-#   return img2
+    response = requests.post(url, files=files)
 
+    print(response)
 
-# if uploaded_file is not None:
-#     #src_image = load_image(uploaded_file)
-#     image = Image.open(uploaded_file)
+    response
 
-#     st.image(uploaded_file, caption='Input Image', use_column_width=True)
-#     #st.write(os.listdir())
-#     im = imgGen2(uploaded_file)
-#     st.image(im, caption='ASCII art', use_column_width=True)
+    if response.status_code == 200:
+        resp = response.content
+
+        with open("sample_image.png", "wb") as file:
+            file.write(response.content)
+
+        image = Image.open(io.BytesIO(response.content))
+        st.image(image, caption='prediction', use_column_width=False)
+
+    else:
+        "😬 api error 🤖"
